29476b168c8726cfc7ae43063d29d5b88c829438
jecerbenhammouda <jasser.benhammouda@esprit.tn>
2023-03-22 14:02:46 +0100

    Project Boot

---
+++ src/main/java/tn/infotunis/InfotunisApplication.java
@@ -0,0 +1,13 @@
+package tn.infotunis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfotunisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfotunisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/infotunis/InfotunisApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.infotunis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfotunisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
